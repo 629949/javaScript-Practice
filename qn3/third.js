dd1d183e28aadcade7c8d35a1b8f3c8f0600c799
GADOBUA <emmanuelobua54@gmail.com>
2024-04-17 23:52:47 +0300

    practice files

--- qn3/third.js
+++ qn3/third.js
@@ -6,3 +6,4 @@ function twoSum() {
 }
 
 console.log(twoSum());
+
--- qn5/fifth.js
+++ qn5/fifth.js
@@ -1,3 +1,17 @@
-function num() {
-    myNumber
+function sumToOneDigit(number) {
+    // Function to calculate the sum of digits in a number
+    function digitSum(num) {
+        return num.toString().split('').reduce((acc, digit) => acc + parseInt(digit), 0);
+    }
+
+    // Keep looping until the number becomes a single digit
+    while (number >= 10) {
+        number = digitSum(number);
+    }
+    
+    // Return the final single digit
+    return number;
 }
+
+// Test the function
+console.log(sumToOneDigit(567)); 
